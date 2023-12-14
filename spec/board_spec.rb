aac01aa25a0b9dc37235b0b354bfaead50eefb1e
Olive Nives <olivenives63@gmail.com>
2023-12-14 16:57:48 +0300

    Expand test case to 3,3 grid

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -10,6 +10,15 @@ describe Board do
     it "returns true when the board is full" do
       subject = Board.new
       subject.place_letter("X", 0, 0)
+      subject.place_letter("X", 0, 1)
+      subject.place_letter("X", 0, 2)
+      subject.place_letter("X", 1, 0)
+      subject.place_letter("X", 1, 1)
+      subject.place_letter("X", 1, 2)
+      subject.place_letter("X", 2, 0)
+      subject.place_letter("X", 2, 1)
+      subject.place_letter("X", 2, 2)
+      
       expect(subject.game_over?).to eql(true)
     end
   end
