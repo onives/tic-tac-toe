01c247fba60b13729058f4cb812015b746bbb673
Olive Nives <olivenives63@gmail.com>
2023-12-20 15:36:27 +0300

    remove previous make move and player input methods & specs

--- lib/player_functions.rb
+++ lib/player_functions.rb
@@ -1,21 +1,3 @@
-def get_player_input(input, player, board=Array.new(3){Array.new(3, "_")})
-    puts "Player #{player}, enter your placeholder in format num1, num2"
-    input = input.gets.chomp
-
-    row, col = input.split(',').map(&:to_i)
-
-    #TODO Move the call to make_move outside of this function
-    make_move(row, col, player, board)
-end
-
-def make_move(row, col, player, board=Array.new(3){Array.new(3, "_")})
-    if board[row][col] == "_"
-        board[row][col] = player
-    else
-        false
-    end
-end
-
 def switch_player(player)
     if player == "X"
         "O"
--- spec/player_functions_spec.rb
+++ spec/player_functions_spec.rb
@@ -1,35 +1,6 @@
 require './lib/player_functions.rb'
 require 'stringio'
 
-
-describe "#get_user_input" do
-    let(:board){Array.new(3){Array.new(3, "_")}}
-    it "should record player X's input in the board" do
-        input = StringIO.new("1,1\n")
-        get_player_input(input, "X", board)
-        expected = [['_', '_', '_'],['_', 'X', '_'], ['_', '_', '_']]
-        expect(board).to eq(expected)
-    end
-    it "should record player O's input in the board" do
-        input = StringIO.new("0,0\n")
-        get_player_input(input, "O", board)
-        expected =  [['O', '_', '_'],['_', '_', '_'], ['_', '_', '_']]
-        expect(board).to eq(expected)
-    end
-    it "should record second move for X" do
-        input = StringIO.new("0,1\n")
-        board = [['O', '_', '_'],['_', '_', '_'], ['_', '_', '_']]
-        get_player_input(input, "X", board)
-        expected =  [['O', 'X', '_'],['_', '_', '_'], ['_', '_', '_']]
-        expect(board).to eq(expected)
-    end
-    it "should return false when move made already has a xter" do
-        input = StringIO.new("0,1\n")
-        board = [['O', 'X', '_'],['_', '_', '_'], ['_', '_', '_']]
-        expect(get_player_input(input, "O", board)).to eql(false)
-    end
-end 
-
 describe "#switch player" do
     it "should switch player to O" do
         player = "X"
