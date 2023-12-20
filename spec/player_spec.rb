09c66625827bc658e841ab0c56832d7ffde4c746
Olive Nives <olivenives63@gmail.com>
2023-12-20 16:43:29 +0300

    update specs get_player_input

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,16 +6,20 @@ describe Player do
     describe "#get_player_input" do 
         let(:subject){Player.new('X')}
         let(:board){Board.new}
-        it "should get player X's input" do
-            input = StringIO.new("1,1\n")
-            console = StringIO.new
+        let(:console){StringIO.new}
+        let(:input){StringIO.new("1,1\n")}
 
+        it "should get player X's input" do
             result = subject.get_player_input(input, console, board)
             expected = [1, 1]
+    
+            expect(result).to eq(expected)
+        end
+        it "should prompt player for input" do
+            result = subject.get_player_input(input, console, board)
             expected_output = "Player X, enter your placeholder in format num1, num2\n"
             
             expect(console.string).to eq(expected_output)
-            expect(result).to eq(expected)
         end
     end
 end
