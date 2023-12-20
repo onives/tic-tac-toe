25aa1861fa99aa61a74adee36e96de3bc0b8857b
Olive Nives <olivenives63@gmail.com>
2023-12-20 13:28:07 +0300

    get player x's input

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,8 @@
 class Player 
-    def get_player_input
+
+    def get_player_input(input, player, board)
+        puts "Player #{player}, enter your placeholder in format num1, num2"
+        input = input.gets.chomp
+        row, col = input.split(',').map(&:to_i)
     end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,17 @@
 require './lib/player.rb'
+require './lib/board.rb'
 require 'stringio'
 
 describe Player do
     describe "#get_player_input" do 
-        it "exists" do 
-            subject = Player.new
-            subject.get_player_input
+        let(:subject){Player.new}
+        let(:board){Board.new}
+        it "should get player X's input" do
+            input = StringIO.new("1,1\n")
+            result = subject.get_player_input(input, "X", board)
+            expected = [1, 1]
+
+            expect(result).to eq(expected)
         end
     end
 end
