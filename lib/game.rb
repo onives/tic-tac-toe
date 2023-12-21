3546af75d95e294dc95d1b47c4086be145b092c7
Olive Nives <olivenives63@gmail.com>
2023-12-21 15:19:53 +0300

    add play_game method to game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,33 @@
+require 'player.rb'
+require 'stringio'
+
 class Game
 
-    def initialize(board)
+    def initialize(board: Board.new, player: Player.new('X'), input: StringIO.new("0,0\n"), output: StringIO.new)
         @board = board
+        @player = player
+        @input = input
+        @output = output
+        
     end
 
     def switch_player
     end
 
     def play_game
-        @board.display_board
+        @board.display_board(@output)
+        @player.get_player_input(@input, @output)
+
     end
 
 end
+
+
+# display board
+#request player for input
+# take player's input 
+# update the board for with player's letter
+#check for winner
+# switch players
+
+# repeat 1-5
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,16 +6,23 @@ require './lib/board.rb'
 describe Game do
     describe "#switch_player" do 
         let(:board){double("board")}
+        let(:player){double("player")}
         it "exist" do
-            subject = Game.new(board)
+            subject = Game.new
             subject.switch_player
         end
     end
     describe "#play_game" do 
         let(:board){double("board")}
+        let(:player){double("player")}
         it "should display the game board" do 
             expect(board).to receive(:display_board)
-            subject = Game.new(board)
+            subject = Game.new(board: board)
+            subject.play_game
+        end
+        it "should request user for input" do 
+            expect(player).to receive(:get_player_input)
+            subject = Game.new(player: player)
             subject.play_game
         end
     end
