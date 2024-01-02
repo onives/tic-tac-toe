9a76c1aa0d91ad4dab98b11ebfc17d5a07838333
Olive Nives <olivenives63@gmail.com>
2024-01-02 11:50:06 +0300

    Add game loop

--- lib/game.rb
+++ lib/game.rb
@@ -12,12 +12,20 @@ class Game
     end
 
     def play_game
-        @board.display_board(@output)
-        row, col = current_player.get_player_input(@input, @output)
-        @board.make_move(row, col, current_player.player)
-        @board.find_winner
-        switch_player
-        row, col = current_player.get_player_input(@input, @output)
+        loop do
+            @board.display_board(@output)
+            row, col = current_player.get_player_input(@input, @output)
+            if @board.make_move(row, col, current_player.player)
+                winner = @board.find_winner
+                if winner == 'X' or winner == 'O'
+                    break
+                elsif winner == :tie
+                    break
+                else
+                    switch_player
+                end
+            end
+        end
     end
 
     private
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,14 @@ require 'stringio'
 require './lib/game.rb'
 require './lib/board.rb'
 
+def ideas_for_making_test_doubles_change_behavior_over_time
+    #one possible syntax
+    allow(board).to receive(:find_winner).and_return false #return false the first time #find_winner is called
+    allow(board).to receive(:find_winner).and_return 'X' #return 'X' the second time #find_winner is called
+
+    #another possible syntax
+    allow(board).to receive(:find_winner).and_return [false, 'X'] #return false the first time #find_winner is called and return 'X' the second time it is called
+end
 
 describe Game do
     describe "#play_game" do 
@@ -11,35 +19,34 @@ describe Game do
         let(:playerX){double("player_x", :player => 'X', :get_player_input => '')}
         let(:playerO){double("player_o", :player => 'O', :get_player_input => '')}
         it "should display the game board" do
-            board = double("board", :make_move => '', :find_winner => '')
+            board = double("board", :make_move => '', :find_winner => 'X')
             expect(board).to receive(:display_board)
             subject = Game.new(board, [playerX, playerO], input, output)
             subject.play_game
         end
         it "should request user for input" do 
-            board = double("board", :find_winner => '')
-            allow(board).to receive(:display_board)
-            allow(board).to receive(:make_move)
+            board = double("board", :display_board => '', :make_move => '', :find_winner => 'X')
             expect(playerX).to receive(:get_player_input)
             subject = Game.new(board, [playerX, playerO], input, output)
             subject.play_game
         end
         it "should update board with player's input" do 
-            board = double("board", :display_board => '', :find_winner => '' )
+            board = double("board", :display_board => '', :find_winner => 'X', :make_move => '')
             allow(playerX).to receive(:get_player_input){[1, 0]}
             expect(board).to receive(:make_move).with(1, 0, 'X')
             subject = Game.new(board, [playerX, playerO], input, output)
             subject.play_game
         end
         it "should check for winner" do 
-            board = double("board", :display_board => '', :make_move => '' )
+            board = double("board", :display_board => '', :make_move => '', :find_winner => 'X' )
             expect(board).to receive(:find_winner)
 
             subject = Game.new(board, [playerX, playerO], input, output)
             subject.play_game
         end
         it "should be able to switch players" do 
-            board = double("board", :display_board => '', :make_move => '', :find_winner => '' )
+            board = double("board", :display_board => '', :make_move => '' )
+            allow(board).to receive(:find_winner).and_return(false, 'X')
             expect(playerX).to receive(:get_player_input)
             expect(playerO).to receive(:get_player_input)
             subject = Game.new(board, [playerX, playerO], input, output)
